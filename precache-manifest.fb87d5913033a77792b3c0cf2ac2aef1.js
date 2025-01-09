a7d7ea9a0f13c83758d03be854874088db6827bc
aarushgupta1 <88345028+aarushgupta1@users.noreply.github.com>
2025-01-08 21:11:13 -0500

    Add files via upload

---
+++ precache-manifest.fb87d5913033a77792b3c0cf2ac2aef1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d5204b10a8d310e248976e34859b4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7eaf11605dc0a69b7d",
+    "url": "/static/js/2.c67febb1.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.c67febb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be098861ce486bee7a83",
+    "url": "/static/js/3.f3b752c0.chunk.js"
+  },
+  {
+    "revision": "6ff9a19b59560df816f9",
+    "url": "/static/js/main.824872d4.chunk.js"
+  },
+  {
+    "revision": "dfdfa0cdbb2b4cf277e0",
+    "url": "/static/js/runtime-main.667ffb37.js"
+  },
+  {
+    "revision": "0e3b164b17bdc1982ce86f88d35b8ab6",
+    "url": "/static/media/facebook-icon.0e3b164b.jpg"
+  },
+  {
+    "revision": "faddf830e6a3df1553a213df8fec916f",
+    "url": "/static/media/gnitou-and-children.faddf830.jpg"
+  },
+  {
+    "revision": "bfaf2ede40d1c306f81ef8a19ad45e31",
+    "url": "/static/media/gnitou-and-friend.bfaf2ede.jpg"
+  },
+  {
+    "revision": "b0d510ea21c651a1411fa90013e446f2",
+    "url": "/static/media/gnitou-and-student.b0d510ea.jpg"
+  },
+  {
+    "revision": "3f14e1cd87c2def0bd2dbd41bbe956d5",
+    "url": "/static/media/instagram-icon.3f14e1cd.jpg"
+  },
+  {
+    "revision": "f0e4cd0e874e05574275dbd9af166b8d",
+    "url": "/static/media/tiktok-icon.f0e4cd0e.jpg"
+  }
+]);
