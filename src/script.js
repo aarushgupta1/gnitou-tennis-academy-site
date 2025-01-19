bd80de345b71480eea343d0ae7c65ad3601f2dbb
aarushgupta1 <aarushgupta1@users.noreply.github.com>
2025-01-19 14:13:28 +0000

    deploy: a0a6afab126d5eede4a01981c8eb69e7206ea408

---
+++ src/script.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
